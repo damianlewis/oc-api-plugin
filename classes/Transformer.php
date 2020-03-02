456daad41a95ddd7c216652ff23ebd962306c005
Damian Lewis <damian@damianlewis.co.uk>
2020-03-02 11:02:55 +0000

    Allow collection of files to be transformed

--- classes/Transformer.php
+++ classes/Transformer.php
@@ -5,10 +5,18 @@ declare(strict_types=1);
 namespace DamianLewis\Api\Classes;
 
 use League\Fractal\TransformerAbstract;
+use October\Rain\Database\Collection;
 use System\Models\File;
 
 abstract class Transformer extends TransformerAbstract
 {
+    /**
+     * Transforms a file using the given transformer.
+     *
+     * @param  File|null  $file
+     * @param  Transformer  $transformer
+     * @return array|null
+     */
     protected function transformFile(?File $file, Transformer $transformer): ?array
     {
         if ($file === null) {
@@ -16,5 +24,17 @@ abstract class Transformer extends TransformerAbstract
         }
 
         return $transformer->transform($file);
+    }
+
+    /**
+     * Transforms a collection of files using the given transformer.
+     *
+     * @param  Collection  $collection
+     * @param  Transformer  $transformer
+     * @return array
+     */
+    protected function transformFiles(Collection $collection, Transformer $transformer): array
+    {
+        return $collection->map([$transformer, 'transform'])->all();
     }
 }
